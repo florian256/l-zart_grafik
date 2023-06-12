7775f9fa9da52b63fd205cbd3aa20de0d4826231
nicepowpowloko <90335368+nicepowpowloko@users.noreply.github.com>
2023-06-12 10:04:25 +0200

    12/06/23

--- slide-1.js
+++ slide-1.js
@@ -4,25 +4,26 @@ const suivant = document.querySelector('.right');
 const precedent = document.querySelector('.left');
 let count = 0;
 
-function slideSuivante(){
-    items [count].classList.remove('active');
-    if(count < nbSlide -1){
+function slideSuivante() {
+    items[count].classList.remove('active');
+    if (count < nbSlide - 1) {
         count++;
-    } else { 
+    } else {
         count = 0;
     }
-    items[count].classList.add('active')
+    items[count].classList.add('active');
 }
 
-suivant.addEventListener('click' , slidesuivante)
-function slideprecedent(){
+suivant.addEventListener('click', slideSuivante);
+
+function slideprecedent() {
     items[count].classList.remove('active');
-    if (count > 0){
-        cont--;
-
-    } else{
-        count = nbSlide -1;
+    if (count > 0) {
+        count--;
+    } else {
+        count = nbSlide - 1;
     }
-    items[count].classList.add('active')
+    items[count].classList.add('active');
 }
-precedent.addEventListener('click' , slideprecedent)
+
+precedent.addEventListener('click', slideprecedent);
